d6a22b6dcb14836b795525b7cf3b0365282c23e1
Vinod Jantikar <vinodjantikar999@gmail.com>
2023-03-24 19:04:34 +0530

    feat: fetch data from db

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [taskData, setTaskData] = useState({
@@ -8,15 +8,38 @@ const Home = () => {
     important: "",
   });
 
+  const [users, setUsers] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setTaskData({...taskData, [name]: value})
+    setTaskData({ ...taskData, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(taskData);
   };
+
+  useEffect(() => {
+    getUserData();
+    getTasksData()
+  }, []);
+
+  const getUserData = () => {
+    fetch(`http://localhost:3000/Users`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  };
+
+  const getTasksData = () => {
+    fetch(`http://localhost:3000/Tasks`)
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  };
+
+  console.log(users)
+  console.log(tasks)
 
   return (
     <div>
@@ -52,9 +75,12 @@ const Home = () => {
           <div>
             <select onChange={handleChange} name="user">
               <option value="">select</option>
-              <option value="User 1">User 1</option>
-              <option value="User 2">User 2</option>
-              <option value="User 3">User 3</option>
+
+              {users.map((user) => (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              ))}
             </select>
             <p>Users</p>
           </div>
